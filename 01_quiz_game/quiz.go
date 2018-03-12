fd4fb8502e53986d0d6ab94ecc5dc73d6a646a77
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-03-12 16:57:24 -0500

    Ex_01 Pt.2 solution from page

--- 01_quiz_game/quiz.go
+++ 01_quiz_game/quiz.go
@@ -5,12 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 
-	quizFilename := flag.String("qf", "problems.csv", "a csv file in the format of 'question,answer'")
+	quizFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timerLimit := flag.Int("limit", 30, "Time limit in seconds to answer each question")
 	flag.Parse()
+
+	if *timerLimit < 1 {
+		exit("Failed to run the quiz. Time limit should be bigger than 0s.")
+	}
 
 	file, err := os.Open(*quizFilename)
 	if err != nil {
@@ -29,14 +35,29 @@ func main() {
 		quiz[i] = QuizItem{Question: v[0], Answer: v[1]}
 	}
 
-	var ans string
 	var correctAns int
+	var ans string
+	fmt.Print("Press enter when you are ready to start the quiz...")
+	fmt.Scanln(&ans)
+	timer := time.NewTimer(time.Duration(*timerLimit) * time.Second)
+
+	problemloop:
 	for _, v := range quiz {
 		fmt.Printf("%s: ", v.Question)
-		fmt.Scanln(&ans)
+		answerCh := make(chan string)
+		go func() {
+			fmt.Scanln(&ans)
+			answerCh <- ans
+		}()
 
-		if ans == v.Answer {
-			correctAns++
+		select {
+		case <-timer.C:
+			fmt.Print("\nTime expired. ")
+			break problemloop
+		case ans := <-answerCh:
+			if ans == v.Answer {
+				correctAns++
+			}
 		}
 	}
 
