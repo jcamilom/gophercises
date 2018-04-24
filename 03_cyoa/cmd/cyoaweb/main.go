598fd233ea80f5be4384bfd5a86ed7661694bae9
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-04-23 22:48:26 -0500

    Ex_03 Building the http.Handler

--- 03_cyoa/cmd/cyoaweb/main.go
+++ 03_cyoa/cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	cyoa "github.com/jcamilom/gophercises/03_cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA we application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -23,6 +26,8 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 
 }
--- 03_cyoa/story.go
+++ 03_cyoa/story.go
@@ -2,8 +2,51 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandletTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandletTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose your own adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+        <ul>
+        {{range .Options}}
+            <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+    </body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
@@ -19,7 +62,7 @@ type Story map[string]Chapter
 
 type Chapter struct {
 	Title      string   `json:"title"`
-	Paragraphs []string `json:"paragraphs"`
+	Paragraphs []string `json:"story"`
 	Options    []Option `json:"options"`
 }
 
