a3b0f905c78c1c20eae3d40c321bea492ff9523f
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-02 10:10:38 -0500

    Ex_06 Addpat Stdin for HacerRank

--- 06_hacker_rank/camel_case/main.go
+++ 06_hacker_rank/camel_case/main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter camelCase string: ")
-	st, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	st = strings.TrimSuffix(st, "\n")
+	var st string
+	fmt.Scanf("%s\n", &st)
+
 	numberWords := countCamelCaseWords(st)
-	if numberWords == -1 {
-		fmt.Println("The string provided is empty.")
-	} else {
-		fmt.Println(numberWords)
-	}
+	fmt.Println(numberWords)
 }
 
 func countCamelCaseWords(st string) int {
-	if len(st) == 0 {
-		return -1
-	}
 	count := 0
 	for i, ch := range st {
 		// Check if the char is uppercase or if the first char is lowercase
-		if (ch >= 65 && ch <= 90) || (i == 0 && (ch >= 97 && ch <= 122)) {
+		if (ch >= 'A' && ch <= 'Z') || (i == 0 && (ch >= 'a' && ch <= 'z')) {
 			count++
 		}
 	}
