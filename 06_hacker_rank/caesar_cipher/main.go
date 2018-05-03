b72b0eb1b01a0934d5fe93f602cb7fe43407b90e
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-03 00:45:48 -0500

    Ex_06 Cipher working with just with letters

---
+++ 06_hacker_rank/caesar_cipher/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, k int
+	var s string
+	fmt.Scanf("%d\n", &n)
+	fmt.Scanf("%s\n", &s)
+	fmt.Scanf("%d\n", &k)
+
+	fmt.Println(cipherString(s, k))
+}
+
+func cipherString(st string, k int) string {
+	if k == 0 {
+		return st
+	}
+	runes := []rune(st)
+	for i, ch := range runes {
+		if newCh := ch + rune(k); (newCh >= 'a' && newCh <= 'z') || (newCh >= 'A' && newCh <= 'Z') {
+			runes[i] = newCh
+		} else {
+			runes[i] = newCh - rune(26)
+		}
+	}
+	return string(runes)
+}
