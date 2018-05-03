bf679f9feb0dc046a0a76155793c96aaf3a29a17
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-03 11:44:07 -0500

    Ex_06 Fix sets overlapping

--- 06_hacker_rank/caesar_cipher/main.go
+++ 06_hacker_rank/caesar_cipher/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+const a = 'a'
+const z = 'z'
+const aM = 'A'
+const zM = 'Z'
+
 func main() {
 	var n, k int
 	var s string
@@ -15,19 +20,24 @@ func main() {
 }
 
 func cipherString(st string, k int) string {
+	// In case the shift is bigger than the alphabet width, "normalize" it so
+	// that it's never bigger than the alphabet width
+	k = k % 26
+	// If the shift is zero, do nothing
 	if k == 0 {
 		return st
 	}
 	runes := []rune(st)
 	for i, ch := range runes {
 		// Check if the char is not a letter
-		if !(ch >= 'a' && ch <= 'z') && !(ch >= 'A' && ch <= 'Z') {
+		if min, may := (ch >= a && ch <= z), (ch >= aM && ch <= zM); !min && !may {
 			continue
-		}
-		if newCh := ch + rune(k); (newCh >= 'a' && newCh <= 'z') || (newCh >= 'A' && newCh <= 'Z') {
-			runes[i] = newCh
-		} else {
+		} else if newCh := ch + rune(k); (min && (newCh > z)) || (may && (newCh > zM)) {
+			// Shifts and returns the letter to its own set (a - z) or (A - Z), in case of overflow
 			runes[i] = newCh - rune(26)
+		} else {
+			// Shift the letter
+			runes[i] = newCh
 		}
 	}
 	return string(runes)
