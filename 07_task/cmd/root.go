c39a05357bb9919a03951c8ab6d01ed85ef83fbf
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-03 17:25:01 -0500

    Ex_07 Setting up commands

---
+++ 07_task/cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "07_task",
+	Short: "Task is a CLI task manager",
+}
---
+++ 07_task/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jcamilom/gophercises/07_task/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
