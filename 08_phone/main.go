d385733aae2b1e3f9126df26138616c66e00de36
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-04 11:25:36 -0500

    Drivers & Creating DBs

--- 08_phone/main.go
+++ 08_phone/main.go
@@ -1,8 +1,59 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("\\D")
