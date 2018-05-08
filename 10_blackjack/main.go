7047d2f4abf9f7c0830e1cd242bb66149e079786
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-08 13:16:06 -0500

    Ex_10 Blackjack without AI

--- 10_blackjack/main.go
+++ 10_blackjack/main.go
@@ -17,13 +17,37 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
 func main() {
-	cards := deck.New(deck.Deck(3), deck.Shuffle)
+	theDeck := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var playerH, dealerH Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&playerH, &dealerH} {
+			card, theDeck = draw(theDeck)
+			*hand = append(*hand, card)
+		}
 	}
-	var h Hand = cards[0:3]
-	fmt.Println(h)
+	var input string
+	for input != "s" {
+		fmt.Println("Player: ", playerH)
+		fmt.Println("Dealer: ", dealerH.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, theDeck = draw(theDeck)
+			playerH = append(playerH, card)
+		}
+	}
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player: ", playerH)
+	fmt.Println("Dealer: ", dealerH)
+}
+
+func draw(d []deck.Card) (deck.Card, []deck.Card) {
+	return d[0], d[1:]
 }
