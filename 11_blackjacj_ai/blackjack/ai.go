c882758b874a47aa989f62f73cf7daaa6682dd2d
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-12 15:40:52 -0500

    Ex_11 Refactor

--- 11_blackjacj_ai/blackjack/ai.go
+++ 11_blackjacj_ai/blackjack/ai.go
@@ -3,53 +3,64 @@ package blackjack
 import (
 	"fmt"
 
-	"github.com/jcamilom/gophercises/09_deck"
+	deck "github.com/jcamilom/gophercises/09_deck"
 )
 
 type AI interface {
 	Bet() int
-	Results(hand []deck.Card, dealer []deck.Card)
 	Play(hand []deck.Card, dealer deck.Card) Move
+	Results(hand [][]deck.Card, dealer []deck.Card)
 }
 
-type HumanAI struct{}
+type dealerAI struct{}
 
-func (ar *HumanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+func (ai dealerAI) Bet() int {
+	// noop
+	return 1
+}
+
+func (ai dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	dScore := Score(hand...)
+	if dScore <= 16 || (dScore == 17 && Soft(hand...)) {
+		return MoveHit
+	}
+	return MoveStand
+}
+
+func (ai dealerAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+	// noop
+}
+
+func HumanAI() AI {
+	return humanAI{}
+}
+
+type humanAI struct{}
+
+func (ai humanAI) Bet() int {
+	return 1
+}
+
+func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	for {
-		fmt.Println("Player: ", hand)
-		fmt.Println("Dealer: ", dealer)
+		fmt.Println("Player:", hand)
+		fmt.Println("Dealer:", dealer)
 		fmt.Println("What will you do? (h)it, (s)tand")
 		var input string
 		fmt.Scanf("%s\n", &input)
 		switch input {
 		case "h":
-			return Hit
+			return MoveHit
 		case "s":
-			return Stand
+			return MoveStand
 		default:
 			fmt.Println("Invalid option:", input)
 		}
 	}
 }
 
-func (ai *HumanAI) Bet() int {
-	return 1
-}
-
-func (ai *HumanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+func (ai humanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player: ", hand)
-	fmt.Println("Dealer: ", dealer)
-}
-
-// Filler to be implemented
-
-type Move func(GameState) GameState
-
-func Hit(gs GameState) GameState {
-	return gs
-}
-
-func Stand(gs GameState) GameState {
-	return gs
+	fmt.Println("Player:", hand)
+	fmt.Println("Dealer:", dealer)
 }
