283f95ee9fcfaf0c771a2d5b600fd4cadeeaba30
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-13 12:22:27 -0500

    Ex_11 Fixing Errors

--- 11_blackjacj_ai/blackjack/game.go
+++ 11_blackjacj_ai/blackjack/game.go
@@ -78,6 +78,7 @@ type hand struct {
 
 func deal(g *Game) {
 	playerHand := make([]deck.Card, 0, 5)
+	g.handIdx = 0
 	g.dealer = make([]deck.Card, 0, 5)
 	var card deck.Card
 	for i := 0; i < 2; i++ {
@@ -85,10 +86,6 @@ func deal(g *Game) {
 		playerHand = append(playerHand, card)
 		card, g.deck = draw(g.deck)
 		g.dealer = append(g.dealer, card)
-	}
-	playerHand = []deck.Card{
-		{Rank: deck.Seven},
-		{Rank: deck.Seven},
 	}
 	g.player = []hand{
 		{
@@ -101,6 +98,9 @@ func deal(g *Game) {
 
 func bet(g *Game, ai AI, shuffled bool) {
 	bet := ai.Bet(shuffled)
+	if bet < 100 {
+		panic("bet must be at least 100")
+	}
 	g.playerBet = bet
 }
 
@@ -175,12 +175,11 @@ func MoveSplit(g *Game) error {
 		bet:   g.player[g.handIdx].bet,
 	})
 	g.player[g.handIdx].cards = (*cards)[:1]
-	*cards = (*cards)[0:1]
 	return nil
 }
 
 func MoveDouble(g *Game) error {
-	if len(g.player) != 2 {
+	if len(*g.currentHand()) != 2 {
 		return errors.New("can only double on a hand with 2 cards")
 	}
 	g.playerBet *= 2
--- 11_blackjacj_ai/main.go
+++ 11_blackjacj_ai/main.go
@@ -3,8 +3,23 @@ package main
 import (
 	"fmt"
 
+	deck "github.com/jcamilom/gophercises/09_deck"
 	"github.com/jcamilom/gophercises/11_blackjacj_ai/blackjack"
 )
+
+type basicAI struct{}
+
+func (ai *basicAI) Bet(shuffled bool) int {
+	panic("not implemented")
+}
+
+func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	panic("not implemented")
+}
+
+func (ai *basicAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+	panic("not implemented")
+}
 
 func main() {
 	opts := blackjack.Options{
